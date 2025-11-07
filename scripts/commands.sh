d395fd37d1df72c15d1832ae75419116a6cb012e
Tdahuyou <12875288+tdahuyou@user.noreply.gitee.com>
2025-11-07 08:33:02 +0800

    chore: update 3 files (2025-11-07)

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -24,8 +24,7 @@
 # git fetch origin && git reset --hard origin/main && git clean -fd
 
 # 确保本地是最新的：强制推送本地版本覆盖远程版本
-# git add . && git commit -m "update" && git push origin main --force
-git add . & git commit -m "update" & git push origin main --force
+git add . && git commit -m "update" && git push origin main --force
 
 # 彻底恢复仓库状态
 # git reset --hard HEAD && git clean -fd
