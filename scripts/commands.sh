8925c0f34a283fc6fd307ce5a29cd741f2d59a8b
Tdahuyou <dahuyoutop@gmail.com>
2026-02-24 09:43:32 +0800

    update

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -46,3 +46,6 @@
 
 # 移除所有 .vitepress-pid 模块
 # git rm --cached .vitepress-pid && git commit -m "chore: 停止跟踪 .vitepress-pid 文件" && git push
+
+# 确保远程是最新的：强制读取远程版本覆盖本地版本，并拉取项目并初始化子模块
+# git fetch origin && git reset --hard origin/main && git clean -fd && git submodule update --init
